f400696f3c6ae2ca3a2503e942dcbdfd8f1c09dd
Dhruv <dhruvkapure3@gmail.com>
2025-06-11 22:05:36 +0530

    fixations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,61 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { cn } from "@/lib/utils";
+
+const navItems = [
+  { name: "Home", href: "#home" },
+  { name: "About", href: "#about" },
+  { name: "Skills", href: "#Skills" },
+  { name: "Projects", href: "#projects" },
+  { name: "Contact", href: "#contact" },
+];
 
 function Navbar() {
-  const navItems = [
-    { name: "Home", href: "#home" },
-    { name: "About", href: "#about" },
-    { name: "Skills", href: "#Skills" },
-    { name: "Projects", href: "#projects" },
-    { name: "Contact", href: "#contact" },
-  ];
+  const [isScrolled, setIsScrolled] = React.useState(false);
 
-  return <div></div>;
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 50) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  return (
+    <nav
+      className={cn(
+        "fixed w-full z-40 transition-all duration-300",
+        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+      )}
+    >
+      <div className="container flex items-center justify-between ml-5">
+        <a
+          href="#hero"
+          className="text-xl font-bold text-primary flex items-center"
+        >
+          <span className="relative z-10">
+            <span className="text-glow text-foreground">Dhruv</span> Kapure
+          </span>
+        </a>
+        {/* desktop version */}
+        <div className="hidden md:flex space-x-8 text-lg text-foreground">
+          {navItems.map((item) => (
+            <a
+              href={item.href}
+              className="text-foreground/80 hover:text-primary transition-colors duration-300"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
