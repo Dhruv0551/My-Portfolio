3792eca332ae77c16767bbcbc3b232f045452e0e
Dhruv <dhruvkapure3@gmail.com>
2025-06-13 18:19:20 +0530

    working on skills section

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { redirect } from "react-router-dom";
+import { cn } from "../lib/utils";
 
 const skills = [
   { name: "HTML", category: "frontend" },
@@ -18,13 +20,54 @@ const skills = [
   { name: "Git/GitHub", category: "tools" },
 ];
 
+const categories = ["all", "frontend", "backend", "tools"];
+
 function Skills() {
+  const [activeCat, setActiveCat] = React.useState("all");
+  const fillteredSkills = skills.filter(
+    (skill) => activeCat === "all" || skill.category === activeCat
+  );
+
   return (
     <section id="skills" className="py-24 px-24 relative bg-secondary/30">
       <div className="container mx-auto max-w-5xl">
         <h2 className="text-3xl md:text-4xl font-bold text-center mb-12">
-          My <span>Skills</span>
+          My <span className="text-primary">Skills</span>
         </h2>
+
+        <div className="flex flex-wrap justify-center gap-4 mb-12">
+          {categories.map((category, key) => (
+            <button
+              key={key}
+              onClick={() => {
+                setActiveCat(category);
+              }}
+              className={cn(
+                "px-5 py-2 rounded-full transition-colors duration-300 capitalize",
+                activeCat === category
+                  ? "bg-primary text-primary-foreground"
+                  : "bg-secondary/70 text-foreground hover:bd-secondary"
+              )}
+            >
+              {category}
+            </button>
+          ))}
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {fillteredSkills.map((skill, key) => (
+            <div
+              key={key}
+              className="bg-purple-400/10 backdrop-blur-3xl p-6 rounded-lg shadow-xs card-hover"
+            >
+              <div className="mb-4 text-left">
+                <h3 className="font-semibold text-lg">{skill.name}</h3>
+              </div>
+              <div className="w-full bg-secondary/50  rounded-full overflow-hidden">
+                <div className="bg-primary h-2 rounded-full origin-left animate-[grow_1.5s_ease-out]" />
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
