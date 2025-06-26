7dd51ad39093fe50f9db32a1c8fa183882d66b98
Dhruv <dhruvkapure3@gmail.com>
2025-06-26 16:31:43 +0530

    added vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Main from "./components/Main";
 import NotFound from "./components/NotFound";
 import { Toaster } from "./components/ui/Toaster";
+import { Analytics } from "@vercel/analytics/next";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
+      <Analytics />
     </>
   );
 }
